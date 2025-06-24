1eb6a0fab97868a97d6fd1fe4387b1b51bf53b5d
Dmitriy Paramonov <divniy@gmail.com>
2025-06-24 21:25:54 +0300

    Add cacher tests

--- src/apt-cacher/install.sh
+++ src/apt-cacher/install.sh
@@ -13,11 +13,11 @@ echo "The provided hostname is: $HOSTNAME"
 # These may be useful in instances where the context of the final 
 # remoteUser or containerUser is useful.
 # For more details, see https://containers.dev/implementors/features#user-env-var
-echo "The effective dev container remoteUser is '$_REMOTE_USER'"
-echo "The effective dev container remoteUser's home directory is '$_REMOTE_USER_HOME'"
+# echo "The effective dev container remoteUser is '$_REMOTE_USER'"
+# echo "The effective dev container remoteUser's home directory is '$_REMOTE_USER_HOME'"
 
-echo "The effective dev container containerUser is '$_CONTAINER_USER'"
-echo "The effective dev container containerUser's home directory is '$_CONTAINER_USER_HOME'"
+# echo "The effective dev container containerUser is '$_CONTAINER_USER'"
+# echo "The effective dev container containerUser's home directory is '$_CONTAINER_USER_HOME'"
 
 # cat > /usr/local/bin/hello \
 # << EOF
@@ -29,7 +29,7 @@ echo "The effective dev container containerUser's home directory is '$_CONTAINER
 
 cat > /etc/apt/apt.conf.d/01proxy \
 << EOF
-Acquire::HTTP::Proxy "http://172.17.0.1:3142";
+Acquire::HTTP::Proxy "http://${HOSTNAME}:3142";
 Acquire::HTTPS::Proxy "false";
 EOF
 
